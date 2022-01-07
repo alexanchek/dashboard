e11b09b18ec5789534bad2bdecafe5fd37ea94f5
alexanchek <58663068+alexanchek@users.noreply.github.com>
2022-01-07 12:15:19 +0300

    feat: added sidebar component

--- parts/Main/CategoriesBlock/CategoriesBlock.tsx
+++ parts/Main/CategoriesBlock/CategoriesBlock.tsx
@@ -1,12 +1,43 @@
+import classNames from "classnames";
 import React, { FC } from "react";
-import styles from './CategoriesBlock.module.css';
+import CommonCard from "../../../components/CommonCard/CommonCard";
+import Htag from "../../../components/Htag/Htag";
+import styles from "./CategoriesBlock.module.scss";
+import { CategoriesBlockConfig } from "./CategoriesBlockConfig";
 
 const CategoriesBlock: FC = (): JSX.Element => {
   return (
-  <div className={styles.box}>
-      
-  </div>
-  )
+    <CommonCard className={styles.box}>
+      <Htag tag="h3" className={styles.title}>
+        {" "}
+        Top Categories
+      </Htag>
+      <div className={styles.subtitle}>
+        <span>28 Categories, 1400 Posts</span>
+      </div>
+      <div className={styles.categoriesBox}>
+        {CategoriesBlockConfig.map(({title,subtitle}, index) => {
+          return (
+            <div key={title} className={styles.categoryBox}>
+              <div
+                className={classNames(styles.firstRow, styles.categorieTitle)}
+              >
+                <div>{title}</div>
+                <div>8.2k</div>
+              </div>
+              <div className={styles.secondRow}>
+                <div className={styles.categorieSubtitle}>
+                  {subtitle}
+                </div>
+                <div>+472</div>
+              </div>
+             {index === CategoriesBlockConfig.length-1 ? <></> :  <div className={styles.hr}></div>}
+            </div>
+          );
+        })}
+      </div>
+    </CommonCard>
+  );
 };
 
 export default CategoriesBlock;
---
+++ parts/Main/CategoriesBlock/CategoriesBlockConfig.ts
@@ -0,0 +1,15 @@
+interface CategoriesBlockConfig {
+  title: string;
+  subtitle: string;
+}
+
+export const CategoriesBlockConfig = [
+  { title: "Lifestyle", subtitle: "Dailiy lifestyle articles" },
+  { title: "Tutorials", subtitle: "Coding tutorials" },
+  { title: "Technology", subtitle: "Dailiy technology articles" },
+  { title: "UX design", subtitle: "UX design tips" },
+  { title: "Interaction tips", subtitle: "Interaction articles" },
+  { title: "App development", subtitle: "Mobile development articles" },
+  { title: "Nature", subtitle: "Wildlife animal articles" },
+  { title: "Geography", subtitle: "Geography articles" },
+];
